21b1279795814354cdcfe0f83aa72d18cd140b72
Juan Aguilar <carlos-3196@hotmail.com>
2022-02-09 18:01:24 -0600

    Initial commit

---
+++ backend-assessment/src/test/java/com/backend/backendassessment/BackendAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backendassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
